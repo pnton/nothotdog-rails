03ab9c7fddb7f169ca161ea8dda3526f82c0943d
pnton <pnton@Inspiron7559.localdomain>
2018-09-04 00:37:47 -0700

    Removed cors.rb

--- config/initializers/cors.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-      origins 'http://nothotdog-rails.herokuapp.com'
-      resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
