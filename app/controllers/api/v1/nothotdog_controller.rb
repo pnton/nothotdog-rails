66c19829ec51b741f17911d5ca65a3073450225d
Peter Ton <pnton@ucsd.edu>
2019-04-26 05:17:39 -0700

    Changed IP Address

--- app/controllers/api/v1/nothotdog_controller.rb
+++ app/controllers/api/v1/nothotdog_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NothotdogController < ApplicationController
   def is_hot_dog
     response = RestClient::Request.execute(
       method: :post,
-      url: 'jmlxndjp1pcbj.gradient.paperspace.com:5000/is-hot-dog',
+      url: 'jsum7itml9wo07.gradient.paperspace.com:5000/is-hot-dog',
       user: 'peterton',
       password: 'elumitas',
       payload: { :file => params[:file] }
