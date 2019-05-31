eabb00bf9d790d3d9c826ab5cb1ee730b0d8aa36
Peter Ton <pnton@ucsd.edu>
2019-05-31 11:53:46 -0700

    dpg

--- app/controllers/api/v1/nothotdog_controller.rb
+++ app/controllers/api/v1/nothotdog_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NothotdogController < ApplicationController
   def is_hot_dog
     response = RestClient::Request.execute(
       method: :post,
-      url: 'jovadmek12p6t.gradient.paperspace.com:5000/is-hot-dog',
+      url: 'js9gwx7qxedhsr.gradient.paperspace.com:5000/is-hot-dog',
       user: 'peterton',
       password: 'elumitas',
       payload: { :file => params[:file] }
